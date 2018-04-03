4c5f8df762d6c189f4be80042f33c088aa50797a
Brock Tillotson <brock.m.tillotson@gmail.com>
2018-04-02 19:10:16 -0700

    interface coming together

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { formatTweet, formatDate } from '../utils/helpers'
 
 class Timeline extends React.Component {
   render(){
-    console.log("yoo",this.props.tweets)
+    const tweets = this.props.tweets
+    console.log(tweets);
     return (
       <div>
-        <h1>Your Timeline</h1>
-        
+        <div className="time-line">
+          <h1>Your Timeline</h1>
+          {tweets.map((tweet) => (
+            <div className='tweet-box' key={tweet.id}>
+              <div className='avatar-box'>
+                <img className="avatar" src={tweet.avatar} alt={`Avatar for ${tweet.name}`} />
+              </div>
+              <div className='tweet-content'>
+                <p className='tweet-name'>{tweet.name}</p>
+                <p>{formatDate(tweet.timestamp)}</p>
+                <p>{tweet.text}</p>
+                <p>{tweet.parent ? 'replying to @' + tweet.parent.author : null}</p>
+                <p>Liked?{tweet.hasLiked ? " yes" : " no"}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     )
   }
@@ -17,8 +34,17 @@ class Timeline extends React.Component {
 function mapStateToProps({ authedUser, tweets, users} ){
 
   return {
+    tweets: Object.keys(tweets).map((id) => {
+      return (
+      formatTweet(
+        tweets[id],
+        users[tweets[id].author],
+        authedUser,
+        tweets[tweets[id].replyingTo]
+      )
+    )
+  }).sort((a,b) => b.timestamp - a.timestamp),
    authedUser,
-   tweets,
    users
  }
 }
